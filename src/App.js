02fd3ae837c06246ec1a474d88655931d96ca864
Anthony Moss <asmoss93@gmail.com>
2019-04-28 16:19:06 -0400

    creates app as own class and adds message on page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,29 @@ import React from 'react';
 import './App.css';
 import ElapsedTime from "./ElapsedTime";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello There!</p>
-        <div>
-          <ElapsedTime />
-        </div>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      seconds: 0
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <p>Hello There!</p>
+          <h1>
+            Click the buttons to use the Timer
+          </h1>
+          <div>
+            <ElapsedTime seconds={0}/>
+          </div>
+        </header>
+      </div>
+    );
+  }
 }
 
-export default App;
+  export default App;
